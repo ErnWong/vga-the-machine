42981f78019597f7bfc7d3f749696aeb40a4da5c
Ernest Wong <ewon521@gmail.com>
2018-02-04 22:30:59 +1300

    Add verifiable temporary test code

--- src/main.c
+++ src/main.c
@@ -1,4 +1,15 @@
+#include <conio.h>
+#include <graph.h>
+
 int main()
 {
+  _setvideomode(_VRES16COLOR);
+  _setcolor(1);
+  _floodfill(0, 240, -1);
+  _setcolor(15);
+  _settextposition(10, 30);
+  _outtext("hello world!");
+  getch();
+  _setvideomode(_DEFAULTMODE);
   return 0;
 }
